c0f17577e96ed1c42b0bb2f770a6d1518b58da73
Jane Costa <yberdysh@gmail.com>
2019-03-29 19:26:44 -0400

    created users and characters get routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(name: "Jane")
-User.create(name: "Joe")
+require 'faker'
+Character.destroy_all
+Team.destroy_all
+User.destroy_all
+House.destroy_all
+Position.destroy_all
+
+jane = User.create(name: "Jane")
+joe = User.create(name: "Joe")
+
+Team.create(name: "Doggies", user: joe)
+Team.create(name: "KittyKats", user: jane)
+
+houses = ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+positions = ["Beater", "Seeker", "Chaser", "Keeper"]
+
+houses.each do |house|
+  House.create(name: house)
+end
+
+positions.each do |position|
+  Position.create(name: position)
+end
+
+15.times do
+  Character.create(name: Faker::Movies::HarryPotter.unique.character, house: House.all.sample, team: Team.all.sample, position: Position.all.sample)
+end
